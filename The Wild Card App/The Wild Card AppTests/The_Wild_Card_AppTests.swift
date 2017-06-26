8d1b478f1b1875cd49f0e628e5c81502ea3eaf7b
Tolga Caner <caner.tolga@gmail.com>
2017-06-26 13:09:17 +0300

    initial

---
+++ The Wild Card App/The Wild Card AppTests/The_Wild_Card_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Wild_Card_AppTests.swift
+//  The Wild Card AppTests
+//
+//  Created by Tolga Caner on 26/06/2017.
+//  Copyright © 2017 Tolga Caner. All rights reserved.
+//
+
+import XCTest
+@testable import The_Wild_Card_App
+
+class The_Wild_Card_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
